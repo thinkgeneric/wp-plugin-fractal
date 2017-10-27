5e0563657a92561d63d1291f301a4d180145587f
eliseoeric <eric.eliseo@gmail.com>
2017-10-27 14:15:42 -0400

    Fix some syntax errors
    This commit makes some small tweaks to some syntax errors.

--- lib/Admin/Importer.php
+++ lib/Admin/Importer.php
@@ -6,17 +6,15 @@ abstract class Importer {
 
 	protected $nonce_name;
 	protected $nonce_action;
-	
-	public function __construct( $nonce_action, $nonce_name )
-	{
+
+	public function __construct($nonce_action, $nonce_name) {
 		$this->nonce_action = $nonce_action;
-		$this->nonce_name = $nonce_name;
+		$this->nonce_name   = $nonce_name;
 	}
 
-	public function boot()
-	{
-		add_action( 'wp_ajax_get_factual_data', array( $this, 'get_factual_data' ) );
-		add_action( 'wp_ajax_import_factual_data', array( $this, 'import_factual_data' ) );
+	public function boot() {
+		add_action('wp_ajax_get_factual_data', [$this, 'get_factual_data']);
+		add_action('wp_ajax_import_factual_data', [$this, 'import_factual_data']);
 	}
 
 	public function get_factual_data() {
@@ -34,13 +32,12 @@ abstract class Importer {
 	public function maybe_import() {
 		$status = '';
 
-		if( ! ( $this->has_valid_nonce( $_POST['import-listing'], 'import-listing' ) && current_user_can( 'manage_options') ) ) {
+		if ( ! ($this->has_valid_nonce($_POST['import-listing'], 'import-listing') && current_user_can('manage_options'))) {
 			$status = 'noauth-import';
 		}
 
-		if( null !== wp_unslash( $_POST['import-listing'] ) ) {
-			$status = $this->import( $_POST['import-rows'] );
-//			$status = $_POST['import-rows'];
+		if (null !== wp_unslash($_POST['import-listing'])) {
+			$status = $this->import($_POST['import-rows']);
 		}
 
 		return $status;
@@ -49,21 +46,19 @@ abstract class Importer {
 	public function maybe_fetch() {
 		$status = '';
 
-		if( ! ( $this->has_valid_nonce( $_POST[$this->nonce_name], $this->nonce_action ) && current_user_can( 'manage_options' ) ) ) {
+		if ( ! ($this->has_valid_nonce($_POST[ $this->nonce_name ], $this->nonce_action) && current_user_can('manage_options'))) {
 			$status = 'noauth';
 		}
 
-		if( null !== wp_unslash( $_POST[$this->nonce_name] ) ) {
-			$status = $this->fetch_data( $_POST['factual_locality'], $_POST['factual_limit'], $_POST['factual_category']);
+		if (null !== wp_unslash($_POST[ $this->nonce_name ])) {
+			$status = $this->fetch_data($_POST['factual_locality'], $_POST['factual_limit'], $_POST['factual_category']);
 		}
 
-//		$this->redirect( $status );
 		return $status;
 	}
 
-	private function has_valid_nonce($nonce_name, $nonce_action)
-	{
-		if( ! wp_verify_nonce( $nonce_name, $nonce_action ) ) {
+	private function has_valid_nonce($nonce_name, $nonce_action) {
+		if ( ! wp_verify_nonce($nonce_name, $nonce_action)) {
 			return false;
 		}
 
